287142514673aa4bc447ac4b85cb480c2ec0f979
vikdenic <vik.denic@gmail.com>
2014-09-02 09:29:58 -0500

    Initial Commit

---
+++ MMHS_Event_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMHS_Event_App
+//
+//  Created by Vik Denic on 9/2/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
