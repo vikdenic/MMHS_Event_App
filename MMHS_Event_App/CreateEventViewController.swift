100253bd64b46f1b99b75e57e427998b15f2b5de
vikdenic <vik.denic@gmail.com>
2014-09-02 14:08:23 -0500

    Implement more viewcontrollers in ib

---
+++ MMHS_Event_App/CreateEventViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateEventViewController.swift
+//  MMHS_Event_App
+//
+//  Created by Vik Denic on 9/2/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
