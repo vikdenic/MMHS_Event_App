5373fd09201743424c94eb243b4ef72d6b883c55
vikdenic <vik.denic@gmail.com>
2014-09-03 13:45:52 -0500

    EditProfileViewController created \ edit profile and cover photo implemented

---
+++ MMHS_Event_App/BlurredImageView.swift
@@ -0,0 +1,28 @@
+//
+//  BlurredImageView.swift
+//  MMHS_Event_App
+//
+//  Created by Vik Denic on 9/3/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class BlurredImageView: UIImageView {
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+
+    required init(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+
+        var blur : UIBlurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
+        var effectView : UIVisualEffectView = UIVisualEffectView(effect: blur)
+        effectView.frame = frame
+        addSubview(effectView)
+    }
+
+}
--- MMHS_Event_App/EditProfileViewController.swift
+++ MMHS_Event_App/EditProfileViewController.swift
@@ -8,17 +8,53 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController {
+class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    let imagePicker = UIImagePickerController()
+    var selectedProfilePic = UIImage()
+    var selectedCoverPhoto = UIImage()
+    var settingProfilePic = Bool()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
     }
 
+    //MARK: image picker
+    @IBAction func onChangePhotoTapped(sender: UIButton)
+    {
+        if sender.tag == 0
+        {
+            settingProfilePic = true
+        }
+        else{
+            settingProfilePic = false
+        }
 
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject])
+    {
+        dismissViewControllerAnimated(true, completion: { () -> Void in
+
+            if self.settingProfilePic == true
+            {
+                self.selectedProfilePic = info[UIImagePickerControllerEditedImage] as UIImage
+            }
+            else{
+                self.selectedCoverPhoto = info[UIImagePickerControllerEditedImage] as UIImage
+            }
+        })
+    }
+
+    //MARK: Bar Button Actions
     @IBAction func onSaveButtonTapped(sender: UIBarButtonItem)
     {
+
     }
 
     @IBAction func onDismissButtonTapped(sender: UIBarButtonItem)
