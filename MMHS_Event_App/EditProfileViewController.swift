68ada1ea0f08d560c524a3a75598af7d96c63d2a
vikdenic <vik.denic@gmail.com>
2014-09-03 15:56:32 -0500

    Implement iCloud discoverability \did so in editViewController for experiment, will now move this to home

--- MMHS_Event_App/EditProfileViewController.swift
+++ MMHS_Event_App/EditProfileViewController.swift
@@ -12,7 +12,7 @@ import CloudKit
 class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var publicDatabase : CKDatabase = CKContainer.defaultContainer().publicCloudDatabase
-    let cloudManager = AAPL
+    let cloudManager = AAPLCloudManager()
 
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var hometownTextField: UITextField!
@@ -23,14 +23,42 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
     var selectedCoverPhoto = UIImage()
     var settingProfilePic = Bool()
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         imagePicker.allowsEditing = true
         imagePicker.delegate = self
 
+        //MARK: CloudManager
+        cloudManager .requestDiscoverabilityPermission { (discoverable) -> Void in
+            if discoverable
+            {
+                self.cloudManager.discoverUserInfo({ (user) -> Void in
+                    self.discoveredUserInfo(user)
+                })
+            } else{
+                let alert = UIAlertController(title: "CloudKit", message: "Getting your name using Discoverability requires permission", preferredStyle: UIAlertControllerStyle.Alert)
 
+                let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (var act) -> Void in
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                })
+                alert.addAction(action)
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+    }
+
+    func discoveredUserInfo(user : CKDiscoveredUserInfo!)
+    {
+        if user != nil
+        {
+            self.nameTextField.text = "\(user.firstName) \(user.lastName)"
+        }
+        else{
+            self.nameTextField.text = "Not signed into iCloud"
+        }
     }
 
     //MARK: image picker
