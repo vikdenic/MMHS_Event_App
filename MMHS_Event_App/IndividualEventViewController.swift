980d47a3e2815d58ca87065546e7f26e3cb4d3c6
vikdenic <vik.denic@gmail.com>
2014-09-08 04:28:37 -0500

    Implement photo taking in individual event photo stream \hooked up camera button and actions

--- MMHS_Event_App/IndividualEventViewController.swift
+++ MMHS_Event_App/IndividualEventViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class IndividualEventViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class IndividualEventViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var tableView: UITableView!
+    let imagePicker = UIImagePickerController()
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.editing = true
+    }
 
+    @IBAction func onCameraButtonTapped(sender: UIBarButtonItem)
+    {
+        actionSheet()
+    }
+
+    func actionSheet()
+    {
+        let actionSheet = UIActionSheet(title: nil, delegate: self, cancelButtonTitle: "Cancel", destructiveButtonTitle: nil, otherButtonTitles: "Camera", "Library")
+
+        actionSheet.showInView(view)
+    }
+
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int)
+    {
+        if buttonIndex == 1
+        {
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            presentViewController(imagePicker, animated: true, completion: nil)
+        } else{
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            presentViewController(imagePicker, animated: true, completion: nil)
+        }
     }
 
     //MARK: TableView
